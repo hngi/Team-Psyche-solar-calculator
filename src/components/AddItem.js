780f1d10d0bacaa19c2c016128aa21fee38e5e15
maryjonah <jonahmary17@gmail.com>
2019-10-04 21:09:20 +0000

    Updated Name of appliance to search and create dropdown

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,13 +1,30 @@
 import React, { useState, useRef } from "react";
 import { Input, Button } from './forms';
 import { Stack } from "./layouts";
+import Select from 'react-select';
+import CreatableSelect from 'react-select/creatable';
+
 // import { log } from "../libs/helpers";
 
 const Label = (props) => <label {...props} className="font-sans"/>
+const appliance = [
+  { label: "Ceiling Fan", value: 1 },
+  { label: "Light Bulb", value: 2 },
+  { label: "Water Heater", value: 3 },
+  { label: "Laptop", value: 4 },
+  { label: "Refrigerator", value: 5 },
+  { label: "Rice Cooker", value: 6 },
+  { label: "Toaster", value: 7 },
+  { label: "Hair Dryer", value: 8 },
+  { label: "Washing Machine", value: 9 },
+  { label: "Iron", value: 10 },
+  { label: "Television", value: 11 },
+  { label: "Printer", value: 12 },
+];
 
 const fields = {
   data: {
-    appliance: "",
+    appliance: appliance[1].label,
     number: 1,
     time: 1,
     energy: 1
@@ -26,6 +43,10 @@ const AddItem = (props) => {
     setState(fields);
   };
 
+  const onSelectChange = ({label}) => {
+    setState({data:{...state.data, appliance:label}})
+  }
+
   const onChange = e => {
     setState({
       data: { ...state.data, [e.target.name]: e.target.value }
@@ -37,15 +58,10 @@ const AddItem = (props) => {
       <Stack>
         <div className="ma3">
           <Label className="">Name of appliance</Label>
-          <Input
-            fullwidth
-            type="text"
-            placeholder="Televsion"
-            required
-            name="appliance"
-            value={state.data.appliance}
-            onChange={onChange}
-          />
+          <br></br>
+            <CreatableSelect defaultValue={state.data.appliance} name="appliance" options={appliance} onChange={onSelectChange} 
+            placeholder='Television'/>
+            
         </div>
         <div className="ma3 dib">
           <Label>Number of Appliance</Label>
